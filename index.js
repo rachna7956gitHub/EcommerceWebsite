f7bfa68bf241f6e61cbaf6881881115a00b45ade
rachna7956gitHub <103995017+rachna7956gitHub@users.noreply.github.com>
2022-08-23 14:03:49 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,14 @@
+const menu = document.getElementById('bar');
+const nav = document.getElementById('navbar');
+const close = document.getElementById('close')
+
+if(menu){
+    menu.addEventListener('click', () => {
+        nav.classList.add('active');
+    })
+}
+if(close){
+    close.addEventListener('click', () => {
+        nav.classList.remove('active');
+    })
+}
